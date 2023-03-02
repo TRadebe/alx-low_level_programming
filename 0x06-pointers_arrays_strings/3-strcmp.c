7ffad4afe22fb1e2d1158d27fbf437d6f6aae997
Themba <thembaradebe172@gmail.com>
2023-03-02 08:12:46 -0800

    task three

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,6 +1,5 @@
 #include "main.h"
 /**
-
 * _strcmp - compares two strings
 * @s1: first string.
 * @s2: second string.
@@ -11,7 +10,6 @@
 int _strcmp(char *s1, char *s2)
 {
 int i = 0, op = 0;
-
 while (op == 0)
 {
 if ((*(s1 + i) == '\0') && (*(s2 + i) == '\0'))
@@ -19,6 +17,5 @@ break;
 op = *(s1 + i) - *(s2 + i);
 i++;
 }
-
 return (op);
 }
