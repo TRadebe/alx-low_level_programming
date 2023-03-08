a0231b99b0b76b85d0e539718f5064318a68b759
Themba <thembaradebe172@gmail.com>
2023-03-08 00:41:27 -0800

    task 3

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
- *_strlen_recursion - length of a string
- *@s: pointer block of memory to fill
- *Return: strlen_recursion
- */
+*_strlen_recursion - length of a string
+*@s: pointer block of memory to fill
+*Return: strlen_recursion
+*/
 
 int _strlen_recursion(char *s)
 {
@@ -14,4 +14,4 @@ return (0);
 
 else
 return (1 + _strlen_recursion(s + 1)); /*Sum 1*/
-}}
+}
