38f204824f6264b3be882354e1b4b5ca6aa6f757
Themba Radebe <thembaradebe172@gmail.com>
2023-04-03 10:30:28 -0700

    pointing to new links and creating heards

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -6,17 +6,17 @@
 
 /**
 * struct listint_s - singly linked list
-* @n: integer
+* @n: integers 
 * @next: points to the next node
 *
-* Description: strct node
+* Description: struct nodes
 */
 
 typedef struct listint_s
 {
- int n;
- struct listint_s *next;
-} listint_t;
+int n;
+struct listint_s *next;
+}listint_t;
 
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
