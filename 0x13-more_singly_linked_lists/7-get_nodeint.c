587614997726c37a7621e87aff5d04b467ae2ee4
Themba Radebe <thembaradebe172@gmail.com>
2023-04-03 11:43:36 -0700

    deleting and targeting specific links

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -11,10 +11,10 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 unsigned int i = 0;
 listint_t *tempo = head;
-while (temp && i < index)
+while (tempo && i < index)
 {
 tempo = tempo->next;
 i++;
 }
-return (temp ? temp : NULL);
+return (tempo ? tempo : NULL);
 }
