26357f2bde23ddc3def450d6630de7a89ac90d04
Themba <Thembaradebe172@gmail.com>
2023-04-14 14:13:06 -0700

    tough

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -12,19 +12,19 @@
 int create_file(const char *filename, char *text_content)
 {
 int fd, res_write, i;
-
+int O_WRONGLY, O_CREAT,O_TRUNC, S_TRUSR
 if (filename == NULL) /* validate filename input */
 return (-1);
 
 /* Open the file with O_CREAT and O_WRONLY, and set the file permissions */
-fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+fd = popen(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 if (fd == -1) /* check for open() error */
 return (-1);
 
 if (text_content != NULL) /* if content is not NULL, write to file */
 {
 for (i = 0; text_content[i] != '\0'; i++)
-			;
+;
 res_write = write(fd, text_content, i);
 if (res_write == -1) /* check for write() error */
 return (-1);
