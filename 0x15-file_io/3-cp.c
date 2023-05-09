f7a682635fc329d7cd25aa5e370e18c09466e947
TRadebe <thembaradebe172@gmail.com>
2023-05-09 06:38:27 -0700

    fix

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,39 +1,77 @@
 #include "main.h"
-
-/**
-* create_file - creates a new file with specified text content
-* @filename: the name of the file to create
-* @text_content: the text to write to the file
-*
-* Return: 1 on success, -1 on failure
-*/
-
-int create_file(const char *filename, char *text_content)
+char *make_buffer(char *file);
+void close_file(int fd);
+char *make_buffer(char *file)
 {
-int fd, res_write, i;
+	char *buffer;
 
-mode_t mode = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH; /*permissions*/
+	buffer = malloc(sizeof(char) * 1024);
 
-if (filename == NULL) /* validate filename input */
-return (-1);
+	if (buffer == NULL)
+	{
+		dprintf(STDERR_FILENO,
+			"Error: Can't write to %s\n", file);
+		exit(99);
+	}
 
-/* Open the file with O_CREAT and O_WRONLY, and set the file permissions */
-fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, mode);
-if (fd == -1) /* check for open() error */
-return (-1);
-
-if (text_content != NULL) /* if content is not NULL, write to file */
-{
-for (i = 0; text_content[i] != '\0'; i++)
-;
-res_write = write(fd, text_content, i);
-if (res_write == -1) /* check for write() error */
-{
-close(fd);
-return (-1);
-}
+	return (buffer);
 }
 
-close(fd); /* close the file descriptor */
-return (1);
+void close_file(int fd)
+{
+	int cl;
+
+	cl = close(fd);
+
+	if (cl == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+		exit(100);
+	}
+}
+
+int main(int argc, char *argv[])
+{
+	int file_from, file_to, rd, wd;
+	char *buffer;
+
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+
+	buffer = make_buffer(argv[2]);
+	file_from = open(argv[1], O_RDONLY);
+	rd = read(file_from, buffer, 1024);
+	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+
+	do {
+		if (file_from == -1 || rd == -1)
+		{
+			dprintf(STDERR_FILENO,
+				"Error: Can't read from file %s\n", argv[1]);
+			free(buffer);
+			exit(98);
+		}
+
+		wd = write(file_to, buffer, rd);
+		if (file_to == -1 || wd == -1)
+		{
+			dprintf(STDERR_FILENO,
+				"Error: Can't write to %s\n", argv[2]);
+			free(buffer);
+			exit(99);
+		}
+
+		rd = read(file_from, buffer, 1024);
+		file_to = open(argv[2], O_WRONLY | O_APPEND);
+
+	} while (rd > 0);
+
+	free(buffer);
+	close_file(file_from);
+	close_file(file_to);
+
+	return (0);
 }
