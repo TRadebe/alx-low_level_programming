1f0ad9f1e4ddcb38784aea7258d5989b37e62366
Themba <thembaradebe172@gmail.com>
2023-05-26 11:58:16 -0700

    retry

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,41 +1,36 @@
 #include "main.h"
 
-
 /**
-* create_file - Creates a file with a given filename and writes a given string
-* to it
-*
-* @filename: the name of the file to create
-* @text_content: the NULL terminated string to write to the file
-*
-* Return: 1 on success, -1 on failure
-*/
-
-int create_file(const char *filename, char *text_content)
+ * append_text_to_file - Appends text at the end of a file.
+ * @filename: The name of the file.
+ * @text_content: The NULL-terminated string to add at the end of the file.
+ *
+ * Return: 1 on success, -1 on failure.
+ *         If filename is NULL, return -1.
+ *         If text_content is NULL, do not add anything to the file.
+ *         Return 1 if the file exists and -1 if the file does not exist or
+ *         if you do not have the required permissions to write the file.
+ */
+int append_text_to_file(const char *filename, char *text_content)
 {
-int fd, i, len = 0;
-
-if (!filename) /* if filename is NULL */
+if (filename == NULL)
 return (-1);
 
-fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
-
-if (fd == -1) /* if the file can not be created */
+FILE *file = fopen(filename, "a");
+if (file == NULL)
 return (-1);
 
-if (text_content) /* if there is text to write to the file */
+if (text_content != NULL)
 {
-for (i = 0; text_content[i]; i++)
-len++;
-}
-
-if (write(fd, text_content, len) == -1) /* write the string to the file */
+if (fputs(text_content, file) == EOF)
 {
-close(fd);
+fclose(file);
 return (-1);
 }
+}
 
-close(fd);
+if (fclose(file) == EOF)
+return (-1);
 
 return (1);
 }
