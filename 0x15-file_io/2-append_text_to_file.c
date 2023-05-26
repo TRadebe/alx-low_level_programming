98e8eddc9b1646e091b525c2664cfb8af76a6d07
Themba <thembaradebe172@gmail.com>
2023-05-26 12:40:16 -0700

    try 46

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,27 +1,30 @@
 #include "main.h"
-
-int append_text_to_file(const char *filename, char *text_content);
-
 /**
- * main - Entry point of the program.
- * @argc: The number of command-line arguments.
- * @argv: An array of strings representing the command-line arguments.
- *
- * Return: 0 on success, 1 on failure.
+ * append_text_to_file - apends text to a file
+ * @filename: pointer to file that will be appended
+ * @text_content: content to be appended to filename
+ * Return: 1 for success and -1 for failure
  */
-
-int main(int argc, char *argv[])
+int append_text_to_file(const char *filename, char *text_content)
 {
-int res;
+	ssize_t fd, wd;
+	 int len = 0;
 
-if (argc != 3)
-{
-fprintf(stderr, "Usage: %s filename text\n", argv[0]);
-exit(1);
-}
+	if (!filename)
+		return (-1);
 
-res = append_text_to_file(argv[1], argv[2]);
-printf("-> %i\n", res);
+	if (text_content != NULL)
+	{
+	while (text_content[len])
+		len++;
+	}
+	fd = open(filename, O_RDWR | O_APPEND);
 
-return (0);
+	wd = write(fd, text_content, len);
+
+	if (fd == -1 || wd == -1)
+		return (-1);
+
+	close(fd);
+	return (1);
 }
