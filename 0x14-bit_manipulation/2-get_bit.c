6e3de1f767cdc6ce309dce27a82149557dfe5614
Themba <thembaradebe172@gmail.com>
2023-05-26 12:37:00 -0700

    try 45

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,19 +1,30 @@
 #include "main.h"
-
 /**
-* get_bit - return value of a bit at the index in  decimal number
-* @n: number to search
-* @index: index of the bit
-*
-* Return:thee value of the bit
-*/
-int get_bit(unsigned long int n, unsigned int index)
+ * append_text_to_file - apends text to a file
+ * @filename: pointer to file that will be appended
+ * @text_content: content to be appended to filename
+ * Return: 1 for success and -1 for failure
+ */
+int append_text_to_file(const char *filename, char *text_content)
 {
-int bit_value;
-/* if statement condition that states what the indwx ahould be */
-if (index > 63)
-return (-1);
-/* the value of the bit should be determined as follows */
-bit_value = (n >> index) & 1;
-return (bit_value);
+	ssize_t fd, wd;
+	 int len = 0;
+
+	if (!filename)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+	while (text_content[len])
+		len++;
+	}
+	fd = open(filename, O_RDWR | O_APPEND);
+
+	wd = write(fd, text_content, len);
+
+	if (fd == -1 || wd == -1)
+		return (-1);
+
+	close(fd);
+	return (1);
 }
