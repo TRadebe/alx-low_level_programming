886637f6d99836ac60a41cdd184fc7296a578c28
Themba <thembaradebe172@gmail.com>
2023-06-19 17:05:11 -0700

    libraries

---
+++ 0x18-dynamic_libraries/3-puts.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include<stdio.h>
+/**
+* main - Entry point
+* @str: string to be printed
+* Return: Always 0 (Success)
+*/
+void _puts(char *str);
+int main(void)
+{
+puts(str);
+return (0);
+}
