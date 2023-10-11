5ca8f9d094eb7f470076f650628b3dd9e2b57662
Themba <thembaradebe172@gmail.com>
2023-10-11 12:26:42 -0700

    search algorithm is fun

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,34 +1,68 @@
 #include "search_algos.h"
 
 /**
- * linear_search - Searches for a value in an array of integers using
- *                 the Linear search algorithm.
+ * recursive_search - Recursively searches for a value in an array using Binary
  *
- * @array: Pointer to the first element of the array.
- * @size: Number of elements in the array.
+ * @array: Input array.
+ * @size: Size of the array.
  * @value: Value to search for.
  *
- * Return: If the value is found, the index of the first occurrence
- *         in the array; otherwise, -1.
+ * Return: Index of the value if found, otherwise -1.
  */
-
-int linear_search(int *array, size_t size, int value)
+int recursive_search(int *array, size_t size, int value)
 {
-	/* Check if the array is NULL */
-	if (array == NULL)
+	size_t half = size / 2; /* Calculate the middle index */
+	size_t i;
+
+	/* Check for invalid input */
+	if (array == NULL || size == 0)
 		return (-1);
 
-	/* Iterate through the array to find the value */
-	for (size_t i = 0; i < size; i++)
-	{
-		/* Print the current element being checked */
-		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
+	/* Display the current array being searched */
+	printf("Searching in array");
 
-		/* Check if the current element is equal to the value */
-		if (value == array[i])
-			return (i); /* Value found, return the index */
-	}
+	for (i = 0; i < size; i++)
+		printf("%s %d", (i == 0) ? ":" : ",", array[i]);
 
-	/* Value not found in the array */
-	return (-1);
+	printf("\n");
+
+	/* Adjust the middle index for arrays with even size */
+	if (half && size % 2 == 0)
+		half--;
+
+	/* Check if the value is found at the middle index */
+	if (value == array[half])
+		return ((int)half);
+
+	/* Recursively search the left or right half based on the value */
+	if (value < array[half])
+		return (recursive_search(array, half, value));
+
+	half++;
+
+	/* Recursively search the right half */
+	return (recursive_search(array + half, size - half, value) + half);
+}
+
+/**
+ * binary_search - Calls recursive_search to return the index of the number.
+ *
+ * @array: Input array.
+ * @size: Size of the array.
+ * @value: Value to search for.
+ *
+ * Return: Index of the value if found, otherwise -1.
+ */
+int binary_search(int *array, size_t size, int value)
+{
+	int index;
+
+	/* Call the recursive_search function */
+	index = recursive_search(array, size, value);
+
+	/* Check if the value is found at the returned index */
+	if (index >= 0 && array[index] != value)
+		return (-1);
+
+	return (index);
 }
